94cc4628f8e23a95a2b709bfda039960458c4390
Emanuele DelBono <emanuele@codiceplastico.com>
2025-06-11 07:57:42 +0200

    it works;

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
-use glutin_window::GlutinWindow;
+use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{
@@ -83,7 +83,7 @@ impl App {
 
 fn main() {
     let opengl = OpenGL::V3_2;
-    let mut window: GlutinWindow = WindowSettings::new("Pong", [512, 342])
+    let mut window: Window = WindowSettings::new("snake", [200, 200])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
@@ -91,25 +91,25 @@ fn main() {
 
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        snake: Snake::new(Position((10.0), (10.0)), Direction::East),
+        snake: Snake::new(Position(10.0, 10.0), Direction::East),
     };
 
-    // let mut events = Events::new(EventSettings::new());
-    // while let Some(e) = events.next(&mut window) {
-    //     if let Some(r) = e.render_args() {
-    //         app.render(&r);
-    //     }
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            app.render(&r);
+        }
 
-    //     if let Some(u) = e.update_args() {
-    //         app.update(&u);
-    //     }
+        if let Some(u) = e.update_args() {
+            app.update(&u);
+        }
 
-    //     if let Some(b) = e.press_args() {
-    //         app.press(&b);
-    //     }
+        if let Some(b) = e.press_args() {
+            app.press(&b);
+        }
 
-    //     // if let Some(b) = e.release_args() {
-    //     //     app.release(&b);
-    //     // }
-    // }
+        // if let Some(b) = e.release_args() {
+        //     app.release(&b);
+        // }
+    }
 }
