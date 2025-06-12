f2b0897aaf5614503d568b20fbf01149d26a008a
Emanuele DelBono <emanuele@codiceplastico.com>
2025-06-12 11:53:51 +0200

    movements

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,23 @@ extern crate piston;
 
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
+use piston::EventLoop;
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{
     Button, Key, PressEvent, ReleaseEvent, RenderArgs, RenderEvent, UpdateArgs, UpdateEvent,
 };
+use rand::Rng;
 
 use piston::window::WindowSettings;
 use std::process;
 
 struct Position(f64, f64);
+
+impl PartialEq for Position {
+    fn eq(&self, other: &Self) -> bool {
+        (self.0 == other.0) && (self.1 == other.1)
+    }
+}
 
 #[derive(PartialEq)]
 enum Direction {
@@ -29,6 +37,21 @@ struct Snake {
     direction: Direction,
 }
 
+struct Food {
+    pos: Position,
+    count: u32,
+}
+
+impl Food {
+    fn next_pos(self: &mut Self) {
+        let mut rng = rand::rng();
+        self.count += 1;
+        let new_x = (rng.random_range(0..60) * 10) as f64;
+        let new_y = (rng.random_range(0..60) * 10) as f64;
+        self.pos = Position(new_x, new_y);
+    }
+}
+
 impl Snake {
     fn new(pos: Position, dir: Direction) -> Snake {
         Snake {
@@ -42,32 +65,46 @@ impl Snake {
 pub struct App {
     gl: GlGraphics,
     snake: Snake,
+    food: Food,
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
         const BACKGROUND: [f32; 4] = [0.1, 0.1, 0.1, 1.0];
-        const FOREGROUND: [f32; 4] = [0.0, 0.4, 1.0, 1.0];
+        const SNAKE: [f32; 4] = [0.0, 0.4, 1.0, 1.0];
+        const FOOD: [f32; 4] = [0.9, 0.1, 0.1, 1.0];
 
         let Position(x, y) = self.snake.head;
         let snake = rectangle::square(x, y, 10.0);
+        let food = rectangle::square(self.food.pos.0, self.food.pos.1, 10.0);
 
         self.gl.draw(args.viewport(), |c, gl| {
             clear(BACKGROUND, gl);
-            rectangle(FOREGROUND, snake, c.transform.trans(0.0, 0.0), gl);
+            rectangle(SNAKE, snake, c.transform.trans(0.0, 0.0), gl);
+
+            rectangle(FOOD, food, c.transform.trans(0.0, 0.0), gl);
         });
     }
     fn update(&mut self, _args: &UpdateArgs) {
         let Position(x, y) = self.snake.head;
 
-        let step = 1.0;
+        let step = 10.0;
 
         match self.snake.direction {
             Direction::North => self.snake.head = Position(x, y - step),
             Direction::East => self.snake.head = Position(x + step, y),
             Direction::South => self.snake.head = Position(x, y + step),
             Direction::West => self.snake.head = Position(x - step, y),
+        }
+
+        println!(
+            "SNAKE: ?{} ?{}   FOOD: ?{} ?{}",
+            self.snake.head.0, self.snake.head.1, self.food.pos.0, self.food.pos.1
+        );
+
+        if self.snake.head == self.food.pos {
+            self.food.next_pos()
         }
     }
 
@@ -111,9 +148,14 @@ fn main() {
     let mut app = App {
         gl: GlGraphics::new(opengl),
         snake: Snake::new(Position(100.0, 100.0), Direction::East),
+        food: Food {
+            pos: Position(500.0, 500.0),
+            count: 1,
+        },
     };
 
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new().max_fps(20).ups(20));
+
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
             app.render(&r);
