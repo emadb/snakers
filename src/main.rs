e4ccdd5b6b1ef12aa2d063c66acb20b71d43b005
Emanuele DelBono <emanuele@codiceplastico.com>
2025-06-18 18:39:12 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,7 @@ pub const WIDTH: f32 = 800.0;
 pub const HEIGHT: f32 = 600.0;
 pub const SNAKE_UPDATE_INTERVAL: f64 = 0.05;
 
-fn conf() -> Conf {
-    Conf {
-        // Il titolo che apparirà nella barra della finestra
-        window_title: "Snakers".to_string(),
-        // La larghezza desiderata per la finestra di gioco
-        window_width: WIDTH as i32,
-        // L'altezza desiderata per la finestra di gioco
-        window_height: HEIGHT as i32,
-        // Opzionale: puoi rendere la finestra non ridimensionabile dall'utente
-        window_resizable: false,
-        ..Default::default() // Usa i valori di default per tutte le altre opzioni
-    }
-}
-
-#[macroquad::main(conf)]
+#[macroquad::main("snakers")]
 async fn main() {
     let mut app = App::new();
     let mut last_update_time = get_time();
